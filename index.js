e9f8205648b139c16de886876e03002c20987a4a
Cabr0050 <97530527+Cabr0050@users.noreply.github.com>
2022-01-18 11:05:56 -0500

    upload JavaScript

---
+++ index.js
@@ -0,0 +1,41 @@
+
+//Task 1
+//Declare the variables a, b, and c
+let a, b, c; 
+
+//If has value 3, b has value 5, and c doesn't have a value
+let a = 3; 
+let b = 5;
+let c; // variable does not have value 
+
+// alert( "description of alerted_value: " + alerted_value ); Alert popup box has to show the following content for each expression (concatenate everything in one long expression using "\n" to make new lines):
+
+let output = '';
+output += 'let a = 3\n';
+output += 'let b = 5\n';
+output += 'let c = undefined\n';
+output += '------\n';
+output += 'a + b' + (a + b) + '\n'; 
+
+alert(output); 
+
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+//Task 2 
